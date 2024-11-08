7a7e2a1c9433ee8b90637835b0e10c1929712a4d
shivuptl06 <shivupatel9540@gmail.com>
2024-11-08 17:59:25 -0500

    Made the Navbar Sticky

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -72,7 +72,7 @@ function Profile() {
   };
 
   return (
-    <div className="flex flex-col items-center p-6 space-y-6 bg-gray-100 w-full min-h-screen overflow-hidden">
+    <div className="flex flex-col items-center p-6 space-y-6 bg-gray-100 w-full overflow-hidden">
       {/* Profile Section */}
       <div className="flex flex-col items-center space-y-4">
         <div className="w-24 h-24 rounded-full bg-gray-200 flex items-center justify-center overflow-hidden">
