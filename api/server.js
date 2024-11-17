4338302708f99cfac329e2d7eba201b9a2315553
shivuptl06 <104269625+shivuptl06@users.noreply.github.com>
2024-11-17 00:48:31 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Replace with your frontend URL
+    origin: "https://blogapp-frontend-awlh.onrender.com", // Replace with your frontend URL
     credentials: true, // Allow cookies to be sent with requests
   })
 );
